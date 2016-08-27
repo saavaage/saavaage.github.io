00d2e2a4635f8c9be20023574a6b9b3a750bdadf
vitaly <savage1974@gmail.com>
2016-08-27 10:37:55 +0300

    Lesson 7 homework task - codewars finished

---
+++ Work_HTML_CSS_JS/JS/goIT/07/home/codwars2.js
@@ -0,0 +1,35 @@
+/**
+ * Created by boss on 26 августа.
+ */
+/*
+ There's no such thing as private properties on a coffeescript object! But, maybe there are?
+
+ Implement a function createSecretHolder(secret) which accepts any value as secret and returns an object with ONLY two methods
+
+ getSecret() which returns the secret
+ setSecret() which sets the secret
+ obj = createSecretHolder(5)
+ obj.getSecret() # returns 5
+ obj.setSecret(2)
+ obj.getSecret() # returns 2
+ */
+
+function createSecretHolder(secret){
+    return {
+        secretValue: secret,
+
+        getSecret: function () {
+            return this.secretValue;
+        },
+
+        setSecret: function (secret) {
+            this.secretValue = secret;
+        }
+    }
+}
+
+obj = createSecretHolder(10);
+
+console.log(obj.getSecret()); //returns 5
+obj.setSecret(2);
+console.log(obj.getSecret()); // returns 2
