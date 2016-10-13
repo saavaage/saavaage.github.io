43a8594a896b11c33f97c85804dde319879d45af
vitaly <savage1974@gmail.com>
2016-10-13 05:10:53 +0300

    tasks of lesson 17-18 partly

---
+++ Work_HTML_CSS_JS/JS/goIT/17-18/home/objectCircle.js
@@ -0,0 +1,36 @@
+/**
+ * Created by boss on 12 октября.
+ */
+
+'use strict';
+var data = {
+    "Рыбы": {
+        "Форель": {},
+        "Щука": {}
+    },
+
+    "Деревья": {
+        "Хвойные": {
+            "Лиственница": {},
+            "Ель": {}
+        },
+        "Цветковые": {
+            "Берёза": {},
+            "Тополь": {}
+        }
+    }
+};
+
+function createTreeText(obj) { // отдельная рекурсивная функция
+    var li = '';
+    for (var key in obj) {
+        li += '<li>' + key + createTreeText(obj[key]) + '</li>';
+        console.log(li);
+    }
+    if (li) {
+        var ul = '<ul>' + li + '</ul>'
+    }
+    return ul || '';
+}
+
+console.log(createTreeText(data));
